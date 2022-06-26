1599df90fc85b8a754b8ea52f47861f531f9fa2b
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-06-26 11:03:51 +0800

    [新增功能]基本Firebase安裝完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:real_weather_shared_app/mainPage/screens/mainScreen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

f5c497f3dc5d559416ed5fe7c8bf437d839e5ff6
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-07-03 13:21:25 +0800

    修正po文時間顯示

--- lib/mainPage/mainPostPage/widgets/locDateWidget.dart
+++ lib/mainPage/mainPostPage/widgets/locDateWidget.dart
@@ -14,10 +14,30 @@ class LocDateWidget extends StatelessWidget {
       required this.postDate})
       : super(key: key);
 
+  String getReadableTime() {
+    var dt = DateTime.fromMillisecondsSinceEpoch(postDate);
+    var duration = DateTime.now().difference(dt);
+    var readableTime = DateFormat('yyyy/MM/dd HH:mm').format(dt);
+    if (duration.inDays == 1) {
+      readableTime = "昨天 ${DateFormat('HH:mm').format(dt)}";
+    }
+    if (duration.inDays == 2) {
+      readableTime = "前天 ${DateFormat('HH:mm').format(dt)}";
+    }
+    if (duration.inHours < 24) {
+      readableTime = "${duration.inHours}小時前";
+    }
+    if (duration.inMinutes < 60) {
+      readableTime = "${duration.inMinutes}分鐘前";
+    }
+    if (duration.inSeconds < 60) {
+      readableTime = "${duration.inSeconds}秒鐘前";
+    }
+    return readableTime;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var dt = DateTime.fromMillisecondsSinceEpoch(postDate);
-    var readableTime = DateFormat('yyyy/MM/dd, HH:mm').format(dt);
     return Row(
       children: [
         SizedBox(
@@ -35,9 +55,10 @@ class LocDateWidget extends StatelessWidget {
           style: TextStyle(fontSize: 15),
         ),
         SizedBox(
-          width: 5,
+          width: 20,
         ),
-        Text(readableTime, style: TextStyle(fontSize: 15))
+        Text(getReadableTime(),
+            style: TextStyle(fontSize: 15, color: Colors.grey))
       ],
     );
   }

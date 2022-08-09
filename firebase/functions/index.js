68bb4ae637d646f3c4ab8b75e67466bdb0742314
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-08-09 21:56:55 +0800

    完成cloud function schedule(自動刪除超過15天的貼文)

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,16 +1,25 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-exports.app = functions.auth.user().onCreate( async (user) => {
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { firestore } = require("firebase-admin");
+admin.initializeApp();
 
-  if (user.providerData.find(d => d && d.providerId === 'facebook.com') || user.providerData === 'facebook.com') {
-    
-      try {
-      await admin.auth().updateUser(user.uid, {
-          emailVerified: true
-        })
-      } catch (err) {
-        console.log('err when verifying email', err)
-      }
+exports.removeExpiredPostAndPic = functions.pubsub.schedule("every 240 hours").onRun(async (context) => {
+  const db = admin.firestore();
+  const now = firestore.Timestamp.now();
+  const ts = now.toMillis() - 1296000000; // 24 hours in milliseconds = 86400000
+
+  const snap = await db.collection("posts").where("postDateTimeStamp", "<", ts).get();
+  let promises = [];
+
+  for(const snapChild of snap.docs){
+    const bucket = admin.storage().bucket();
+    const path = `post_image/${snapChild.data().postId}.jpg`;
+    await bucket.file(path).delete();
+    await db.collection("replies").doc(snapChild.data().postId).delete();
   }
-})
+  snap.forEach((snap) => {
+    promises.push(snap.ref.delete());
+  });
+  return Promise.all(promises);
+});

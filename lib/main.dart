ded601855fbd9cc597f3365efa670781abab463f
Wayne-Wang-Wayne <w702897028970289@gmail.com>
2022-08-14 16:48:00 +0800

    新增返回兩次才會離開App的邏輯

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'package:real_weather_shared_app/mainPage/createPostPage/screens/createPostScreen.dart';
 import 'package:real_weather_shared_app/mainPage/mainPostPage/providers/mainPostPageProvider.dart';
@@ -41,6 +42,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    DateTime? currentBackPressTime;
+    Future<bool> onWillPop() {
+      DateTime now = DateTime.now();
+      if (currentBackPressTime == null ||
+          now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
+        currentBackPressTime = now;
+        Fluttertoast.showToast(msg: "再按一次以離開App");
+        return Future.value(false);
+      }
+      return Future.value(true);
+    }
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<SignInProvider>(create: (_) => SignInProvider()),
@@ -53,22 +66,23 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey)
                 .copyWith(secondary: Colors.white)),
         home: Scaffold(
-          body: StreamBuilder(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasData) {
-                  return MainScreen();
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Oops!有東西出錯了。"),
-                  );
-                } else {
-                  return AuthScreen();
-                }
-              }),
-        ),
+            body: WillPopScope(
+                child: StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasData) {
+                        return MainScreen();
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text("Oops!有東西出錯了。"),
+                        );
+                      } else {
+                        return AuthScreen();
+                      }
+                    }),
+                onWillPop: onWillPop)),
         onGenerateRoute: (route) => onGenerateRoute(route),
         builder: EasyLoading.init(),
       ),
